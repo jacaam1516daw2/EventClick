2349de530be1dbbf9a1f15add85bf333a064b08c
jaimecastignani <jaimecastignani@gmail.com>
2016-05-10 16:44:15 +0200

    Estable 3

--- public/javascripts/view.index.js
+++ public/javascripts/view.index.js
@@ -1,32 +1,8 @@
 $(function () {
-    // $('#init').hide();
-    //$('#alta').fadeIn("slow");
-    $('#alta').hide();
-    $('#add-new').click(function () {
-        $('#init').fadeOut("slow");
-        $('#alta').fadeIn("slow");
-    });
-});
-
-$(function () {
+    /**
+     * Datepicker calendario
+     */
     $(".date").datepicker({
         dateFormat: 'dd/mm/yy'
-    });
-});
-
-$(function () {
-    $('#add-newEvent').click(function () {
-        $('#init').fadeIn("slow");
-        $('#alta').fadeOut("slow");
-        $.ajax({
-            type: "POST",
-            url: "/",
-            data: JSON.stringify({}),
-            contentType: "application/json; charset=utf-8",
-            dataType: "json",
-            success: function (data) {
-                //listaEventClick = data.listaEventClick;
-            },
-        });
     });
 });
--- routes/index.js
+++ routes/index.js
@@ -26,7 +26,39 @@ router.get('/', function (req, res, next) {
 
 });
 
-router.post('/newEvent', function (req, res) {
+/*
+ * Redirección Botones de Inicio
+ */
+router.post('/', function (req, res, next) {
+    MongoClient.connect(url, function (err, db) {
+        assert.equal(null, err);
+        console.log("Connexió correcta");
+        listaEventClick = [];
+        activeEvents(db, err, function () {
+            res.render('index', {
+                title: 'EventClick',
+                listaEventClick: listaEventClick
+            });
+        });
+    });
+
+});
+
+/*
+ * Redirección de Inicio a la pantalla de Altas
+ */
+router.post('/alta', function (req, res) {
+    console.log('alta');
+    res.render('alta', {
+        title: 'EventClick'
+    });
+});
+
+/*
+ * Al guardar el alta nueva guardamos los datos i redireccionamos a la pantalla de Inicio
+ */
+router.post('/insert', function (req, res) {
+    console.log('insert');
     eventClick = new Object();
     eventClick.title = req.body.titleForm;
     eventClick.subtitle = req.body.subtitleForm;
@@ -39,12 +71,71 @@ router.post('/newEvent', function (req, res) {
     MongoClient.connect(url, function (err, db) {
         assert.equal(null, err);
         console.log("Connexió correcta");
-        saveEvents(db, err, function () {});
-        listaEventClick = [];
-        activeEvents(db, err, function () {
-            res.render('index', {
+        saveEvents(db, err, function () {
+            res.redirect('/');
+        });
+    });
+});
+
+/*
+ * Redirección de la pantalla show a la pantalla de edición
+ * Hacemos una busqueda por ID evento para enviar los datos actualizados
+ */
+router.post('/edit', function (req, res) {
+    console.log('edit');
+    eventClick = new Object();
+    eventClick.idEvent = req.body.idEvent;
+    MongoClient.connect(url, function (err, db) {
+        assert.equal(null, err);
+        console.log("Connexió correcta");
+        eventsById(db, err, function () {
+            console.log('eventsById edit: ' + eventClick.title);
+            res.render('edit', {
                 title: 'EventClick',
-                listaEventClick: listaEventClick
+                eventClick: eventClick
+            });
+        });
+    });
+});
+
+/*
+ * Al modificar los datos guardamos los datos y redireccionamos a la pantalla de Inicio
+ */
+router.post('/update', function (req, res) {
+    console.log('update');
+    eventClick = new Object();
+    eventClick.idEvent = req.body.idEvent;
+    eventClick.title = req.body.titleForm;
+    eventClick.subtitle = req.body.subtitleForm;
+    eventClick.description = req.body.descriptionForm;
+    //eventClick.author = req.body.author;
+    eventClick.isActive = req.body.isActiveForm;
+    eventClick.initDate = req.body.initDateForm;
+    eventClick.endDate = req.body.endDateForm;
+
+    MongoClient.connect(url, function (err, db) {
+        assert.equal(null, err);
+        console.log("Connexió correcta");
+        editEvents(db, err, function () {
+            res.redirect('/');
+        });
+    });
+});
+
+/*
+ * Visualización de evento y busqueda por id
+ */
+router.post('/show', function (req, res) {
+    console.log("show");
+    eventClick = new Object();
+    eventClick.idEvent = req.body.idEvent;
+    MongoClient.connect(url, function (err, db) {
+        assert.equal(null, err);
+        console.log("Connexió correcta");
+        eventsById(db, err, function () {
+            res.render('show', {
+                title: 'EventClick',
+                eventClick: eventClick
             });
         });
     });
@@ -54,6 +145,75 @@ router.post('/newEvent', function (req, res) {
  * MONGO ACTIONS
  */
 
+/**
+ * Alta de vento nuevo
+ */
+var saveEvents = function (db, err, callback) {
+    db.collection('events').insertOne({
+        "title": eventClick.title,
+        "subtitle": eventClick.subtitle,
+        "description": eventClick.description,
+        //"author": author,
+        "isActive": eventClick.isActive,
+        "initDate": eventClick.initDate,
+        "endDate": eventClick.endDate
+    });
+    assert.equal(err, null);
+    console.log("Nuevo evento creado en col·lección de eventos");
+    callback();
+};
+
+/**
+ * Modificación evento (guardamos por id)
+ */
+var editEvents = function (db, err, callback) {
+    db.collection('events').updateOne({
+        "title": eventClick.title,
+        "subtitle": eventClick.subtitle,
+        "description": eventClick.description,
+        //"author": author,
+        "isActive": eventClick.isActive,
+        "initDate": eventClick.initDate,
+        "endDate": eventClick.endDate
+    }, {
+        $set: {
+            "_id": ObjectId(eventClick.idEvent)
+        }
+    });
+    assert.equal(err, null);
+    console.log("Evento modificado en col·lección de eventos");
+    callback();
+};
+
+/**
+ * Busqueda evento por ID
+ */
+var eventsById = function (db, err, callback) {
+    console.log("eventsById");
+    var cursor = db.collection('events').find({
+        "_id": ObjectId(eventClick.idEvent)
+    });
+    cursor.each(function (err, doc) {
+        assert.equal(err, null);
+        if (doc != null) {
+            eventClick.idEvent = doc._id;
+            eventClick.title = doc.title;
+            eventClick.subtitle = doc.subtitle;
+            eventClick.description = doc.description;
+            //eventClick.author = doc.author;
+            eventClick.isActive = doc.isActive;
+            eventClick.initDate = doc.initDate;
+            eventClick.endDate = doc.endDate;
+        } else {
+            callback();
+        }
+
+    });
+};
+
+/**
+ * Busqueda evento pantalla de inicio (Solo los activos)
+ */
 var activeEvents = function (db, err, callback) {
     var cursor = db.collection('events').find({
         "isActive": 'on'
@@ -77,21 +237,6 @@ var activeEvents = function (db, err, callback) {
         }
 
     });
-};
-
-var saveEvents = function (db, err, callback) {
-    db.collection('events').insertOne({
-        "title": eventClick.title,
-        "subtitle": eventClick.subtitle,
-        "description": eventClick.description,
-        //"author": author,
-        "isActive": eventClick.isActive,
-        "initDate": eventClick.initDate,
-        "endDate": eventClick.endDate
-    });
-    assert.equal(err, null);
-    console.log("Nuevo evento creado en col·lección de eventos");
-    callback();
 };
 
 module.exports = router;
